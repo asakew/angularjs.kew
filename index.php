996a1f1c04abec47b23e4e9773585694db5ff05d
Akmal Sindorov <asakew@ya.ru>
2021-01-17 19:09:27 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="myApp"  ng-controller="customersCtrl">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>alallaalal</title>
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<script src="assets/js/angular.min.js"></script>
+	</head>
+	<body class="container my-3 bg-dark text-white">
+
+<div ng-include="'include.htm'"></div>
+<div ng-include="'main.html'"></div>
+
+
+	</body>
+</html>
